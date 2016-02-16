b82f3f1c7bf37124221111d2ae36912c6e411f62
metrodigihub <deepankumar.pandurangam@integra.co.in>
2016-02-16 19:05:19 +0530

    Update about.php

--- about.php
+++ about.php
@@ -1,1 +1,28 @@
 
+    <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');  
+    class Users extends CI_Controller {  
+            function __construct()  {  
+                    parent::__construct();  
+                    #$this->load->helper('url');  
+                    $this->load->model('users_model');  
+            }  
+            public function index()  {  
+                    $data['habitat_user_list'] = $this->users_model->get_all_users_habitat();
+                    $this->load->view('show_users', $data); 
+
+    //                $data['metrodigi_user_list'] = $this->users_model->get_all_users_metrodigi();  
+    //		$this->load->view('show_users', $data);
+            } 
+            public function GetUsers()  {  
+                    $data['habitat_user_list'] = $this->users_model->get_all_users_habitat();  
+                    //$this->load->view('show_users', $data); 
+                    $this->output->set_header('Content-Type: application/json; charset=utf-8');
+                    echo json_encode($data);
+
+    //                $this->output->set_output($data);
+    //                $this->response = json_encode($data);
+                    //$data['metrodigi_user_list'] = $this->metrodigi_users_model->get_all_users_metrodigi();  
+                    //$this->load->view('show_users', $data);
+            } 
+
+    }
