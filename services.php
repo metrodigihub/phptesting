4f05c16c358e7f508afc64335db5e51645b887f8
metrodigihub <deepankumar.pandurangam@integra.co.in>
2016-02-19 15:32:15 +0530

    Update services.php

--- services.php
+++ services.php
@@ -1,1 +1,14 @@
 
+//    angular.module('HabitatApp', [])
+//            .controller('HabitatUserCtrl', function ($scope, $http){
+//                $scope.habitatuserinfolist = [];       
+//                $http({
+//                    method: 'POST',
+//                    url: '/CIDB/index.php/users/GetUsersInfo'                
+//                    }).then(function successCallback(response){
+//                       $scope.habitatuserinfolist = angular.fromJson(response.data.habitat_user_info_list); 
+//                    }, function errorCallback(response){
+//                     // called asynchronously if an error occurs
+//                      // or server returns response with an error status.
+//                });            
+//            }); 
