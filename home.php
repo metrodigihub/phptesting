4dd2d4ba7e1b1f2e17c0621b6183b5e3924d2f10
metrodigihub <deepankumar.pandurangam@integra.co.in>
2016-02-20 23:10:49 +0530

    Update home.php

--- home.php
+++ home.php
@@ -223,36 +223,7 @@
         </script>
 	</body>
 </html>
-=head3 _get_table_colspec
 
-	This is the method used to get colspec text of the table.
-
-	Function Name  : _get_table_colspec(<tablefirstrowcontent>)
-	Arguments      : Arg1 => Table with content
-	Return         : Colgroup(Colspec) content
-	Usage          : my $count = _get_table_colspec($tablecnt);
-
-=cut
-
-sub _get_table_colspec {
-	my $cnt = shift;
-	my $cols_count = 0;
-	
-	# Find column count
-	if($cnt =~ m{^(?:(?!</?tr[ >]).)*(<tr[ >](?:(?!</?tr[ >]).)*</tr>)}is) {
-		$cols_count = _column_count_table($1);
-	}
-
-	# Colgroup
-	my $colspec_cnt = qq(<tgroups cols="$cols_count">\n);
-	for(my $i=1;$i<=$cols_count;$i++) {
-		$colspec_cnt .= qq(<colspec colnum="$i" colname="C$i" colwidth="*"/>\n);
-	}
-	$colspec_cnt .= qq(</tgroups>);
-	
-	return $colspec_cnt;
-}
-#=====================================================
 			
 			
 		public function get_all_users_habitat()  {
