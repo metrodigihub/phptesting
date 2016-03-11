fbb3b005e9da1f3045d9e24e06b3076360e52cfd
metrodigihub <deepankumar.pandurangam@integra.co.in>
2016-03-11 13:21:26 +0530

    Update services.php

--- services.php
+++ services.php
@@ -1,17 +1,4 @@
 
-//    angular.module('HApp', [])
-//            .controller('UserCtrl', function ($scope, $http){
-//                $scope.userinfolist = [];       
-//                $http({
-//                    method: 'POST',
-//                    url: '/CIDB/index.php/users/GetUsersInfo'                
-//                    }).then(function successCallback(response){
-//                       $scope.userinfolist = angular.fromJson(response.data._user_info_list); 
-//                    }, function errorCallback(response){
-//                     // called asynchronously if an error occurs
-//                      // or server returns response with an error status.
-//                });            
-//            }); 
 
 <html>
 	<head>
