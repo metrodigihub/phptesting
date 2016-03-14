50c8760d071a3e33d737a806fda7ae6a5a54cd42
metrodigihub <deepankumar.pandurangam@integra.co.in>
2016-03-14 10:03:16 +0530

    Update entity.php

--- entity.php
+++ entity.php
@@ -1,1 +1,12 @@
-sdgs
+angular.module('docsSimpleDirective', [])
+.controller('Controller', ['$scope', function($scope) {
+  $scope.customer = {
+    name: 'Naomi',
+    address: '1600 Amphitheatre'
+  };
+}])
+.directive('myCustomer', function() {
+  return {
+    template: 'Name: {{customer.name}} Address: {{customer.address}}'
+  };
+});
