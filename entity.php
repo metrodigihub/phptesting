1cf701ac7998636401a409fe45dff7d161771747
metrodigihub <deepankumar.pandurangam@integra.co.in>
2016-03-15 14:41:11 +0530

    Update entity.php

--- entity.php
+++ entity.php
@@ -10,3 +10,29 @@ angular.module('docsSimpleDirective', [])
     template: 'Name: {{customer.name}} Address: {{customer.address}}'
   };
 });
+angular.module('myApp', [])
+.run(function($rootScope) {
+    $rootScope.test = new Date();
+})
+.controller('myCtrl', function($scope, $rootScope) {
+  $scope.change = function() {
+        $scope.test = new Date();
+    };
+    
+    $scope.getOrig = function() {
+        return $rootScope.test;
+    };
+})
+.controller('myCtrl2', function($scope, $rootScope) {
+    $scope.change = function() {
+        $scope.test = new Date();
+    };
+    
+    $scope.changeRs = function() {
+        $rootScope.test = new Date();
+    };
+    
+    $scope.getOrig = function() {
+        return $rootScope.test;
+    };
+});
