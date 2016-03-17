1d6424a3560e9411e539c7907548f7fe9161a394
metrodigihub <deepankumar.pandurangam@integra.co.in>
2016-03-17 13:44:30 +0530

    Update services.php

--- services.php
+++ services.php
@@ -23,7 +23,9 @@
         <!--[if IE]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
-        
+        <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
   
         <script>
             $(function(){
