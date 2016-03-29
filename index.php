36520a49971dc7cb7bebb1f974dadbfa97e3c063
metrodigihub <deepankumar.pandurangam@integra.co.in>
2016-03-29 18:22:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ $password = "password";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
+// Create connection
+//$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
