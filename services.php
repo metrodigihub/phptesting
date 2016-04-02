ceec5ad9ce88d793e4ae882ab7c99ed1ee3b1a45
metrodigihub <deepankumar.pandurangam@integra.co.in>
2016-04-02 15:45:16 +0530

    Update services.php

--- services.php
+++ services.php
@@ -1,77 +1,21 @@
 
+package EasyQC::ErrorLog;
+use File::Basename;
+use Win32::OLE;
+use Win32;
+BEGIN { Win32::LoadLibrary("comctl32.dll") }
 
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>CI CRUD</title>
-                           
-                     
-        <!--<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>-->         
-        <!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
-        <!--<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>-->         
-        <!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
-     
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery.min.js" ></script>
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/angular.min.js" ></script>
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/datatable/jquery.dataTables.min.js" ></script>
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/datatable/datatableuser.js" ></script>
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/datatable/datatableuserinfo.js" ></script>
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/datatable/angularjsondatatableuser.js" ></script>
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/datatable/angularjsondatatableuserinfo.js" ></script>
-        
-        <?php echo link_tag('assets/css/datatable/jquery.dataTables.min.css')?>
-        <?php echo link_tag('assets/css/tabs.css')?>
-                
-        <!--[if IE]>
-        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-        <!--[if IE]>
-        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-  
-        <script>
-            $(function(){
-                $('ul.tabs li:first').addClass('active');
-                $('.block article').hide();
-                $('.block article:first').show();
-                $('ul.tabs li').on('click',function(){
-                $('ul.tabs li').removeClass('active');
-                $(this).addClass('active');
-                $('.block article').hide();
-                var activeTab = $(this).find('a').attr('href');
-                $(activeTab).show();
-                return false;
-              });
-          });
-         
-        </script>
-  
-       
-                
-	</head>
-    <body ng-app="HabitatApp">
-        <h2 align="center" style="color: blue"></h2>
-                  
-     <section>
-         <br>
-        <ul class="tabs">
-          <li><a href="#tab1"></a></li>
-          <li><a href="#tab2">USERINFORMATION</a></li>
-          <li><a href="#tab3">ABOUT</a></li>
-        </ul>
-        
-        <section class="block">
-            <article id="tab1">
-                <br>
-                
-            </article>
-         </section>
-      </section>
-            
-        <script>
- 
-        </script>
-	</body>
-</html>
+use Win32::TieRegistry(Delimiter=>"/");
+use Tie::IxHash;
+use URI::Escape;
+use iPerl::Basic qw(_open_file _save_file);
 
-
+use strict;
+use 5.006;
+use warnings;
+our(@ISA, @EXPORT, $VERSION);
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT = qw(CreateErrorLog AddErrWarn GetLineCol GetLineCol1 ParseXML CreateErrorLogfromParseContent GetErrorsFromParseContent CreateErrorLogfromTxtLog ExitSub AddLabel AddSubLabel ErrorReturn CreateErrorLog4ErrorReturn HtmlTableValidate OpenFile1 SaveFile1 AddFolderErr WellformCheck WellformCheck_String);
+$VERSION = "1.0";
+my $linkcount=0;
